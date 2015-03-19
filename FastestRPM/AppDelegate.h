ba2e8a31107afb864d9c51c85ee5bc3967e52bac
Samya <samya89@gmail.com>
2015-03-19 11:02:49 -0700

    Initial Commit

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Samia Al Rahmani on 3/19/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FastestRPM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastestRPM
+//
+//  Created by Samia Al Rahmani on 3/19/15.
+//  Copyright (c) 2015 Samia Al Rahmani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
