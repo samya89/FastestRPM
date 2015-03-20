bc74ad61b31416e8b91ff09c3ad38093f3a72bf4
Samya <samya89@gmail.com>
2015-03-20 12:09:34 -0700

    initial commit

--- FastestRPM/ViewController.h
+++ FastestRPM/ViewController.h
@@ -10,6 +10,11 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *needle;
+
+
+
+
 
 @end
 
--- FastestRPM/ViewController.m
+++ FastestRPM/ViewController.m
@@ -10,14 +10,61 @@
 
 @interface ViewController ()
 
+@property (nonatomic, assign) CGFloat needleStart;
+@property (nonatomic, assign) CGFloat velocityFloat;
+
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.needleStart = 135;
+    self.needle.transform = CGAffineTransformMakeRotation(self.needleStart * M_PI/180);
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(didPan:)];
+    [self.view addGestureRecognizer:panGesture];
 }
+
+- (void)didPan:(id)sender
+{
+    UIPanGestureRecognizer *panRecog = (UIPanGestureRecognizer *)sender;
+    CGPoint vel = [panRecog velocityInView:self.view];
+    self.velocityFloat = sqrt(pow(vel.x, 2) + pow(vel.y, 2));
+    self.needleStart = 135;
+    if (panRecog.state == UIGestureRecognizerStateBegan || panRecog.state == UIGestureRecognizerStateChanged)
+    {
+        [self moveNeedleWithVelocity:self.velocityFloat];
+        
+    } else if (panRecog.state == UIGestureRecognizerStateEnded) {
+        [self moveNeedleWithVelocity:self.needleStart];
+    }
+    
+        NSLog(@"vel: %f", self.velocityFloat);
+}
+
+- (void)moveNeedleWithVelocity:(CGFloat)vel
+{
+    if (vel>4000.f) vel = 4000.f;
+    [self moveNeedleWithPercentage:vel/4000.f];
+}
+
+-(void)moveNeedleWithPercentage:(CGFloat)perc
+{
+    self.needle.transform = CGAffineTransformMakeRotation((135+270.f*perc) * M_PI/180);
+}
+
+
+//
+//- (void)moveNeedle:(CGFloat)velocity
+//{
+//    
+//}
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
